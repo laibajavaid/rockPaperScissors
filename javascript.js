63f801eaec112e5346ab825dfef3d156e789eda0
laibajavaid <laibajavaid0@gmail.com>
2022-12-03 20:35:29 -0500

    buttons and instruction page setup w js and html css

--- javascript.js
+++ javascript.js
@@ -1,6 +1,19 @@
-function getComputerChoice(word) {
-    let choices = ['rock', 'paper', 'scissors']
-    return choices[Math.floor(Math.random() * choices.length)]
-  }
- 
-console.log(getComputerChoice())
+let startContainer = document.getElementById('starter');
+let btnOne = document.getElementById('startButton');
+let btnTwo = document.getElementById('instructionButton');
+let btnThree = document.getElementById('homepage')
+let instruction = document.getElementById('instructions');
+
+btnOne.addEventListener('click', () => {
+    startContainer.style.display = 'none';
+});
+
+btnTwo.addEventListener('click', () => {
+    startContainer.style.display = 'none';
+    instruction.style.display = 'block';
+});
+
+btnThree.addEventListener('click', () => {
+    startContainer.style.display = 'block';
+    instruction.style.display = 'none';
+});
